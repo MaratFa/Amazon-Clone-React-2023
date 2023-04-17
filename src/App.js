b7e5dc2c9405c8eb9963d4b263e7c7666c6fcf1e
Marat <evolution99@rambler.ru>
2023-04-17 19:48:30 +0300

    Clear home page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css'
 
 function App() {
   return (
+    // Bem
     <div className="App">
         <h1>Hello! Let's build the Amazon Store</h1>
+        {/* Header */}
+        {/* Home */}
     </div>
   );
 }
