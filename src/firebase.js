047f03ad28807dcfde6a6729b4004baa2a0941dd
Marat <evolution99@rambler.ru>
2023-04-17 06:32:37 +0300

    Adding firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFyZNW98fh6cODARz8eqAGa-iP5J8ib4o",
+  authDomain: "chalenge-d57e1.firebaseapp.com",
+  projectId: "chalenge-d57e1",
+  storageBucket: "chalenge-d57e1.appspot.com",
+  messagingSenderId: "559093158694",
+  appId: "1:559093158694:web:45a1c603e17f28019ca7e7",
+  measurementId: "G-FGM65E2W4W"
+};
