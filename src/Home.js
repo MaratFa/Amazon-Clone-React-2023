abaac8e9a4d6c9a711ffbc4351f3be831eed21e4
Marat <evolution99@rambler.ru>
2023-04-19 23:54:05 +0300

    Adding 'Home.js'

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/71tIrZqybrL._SX3000_.jpg"
+          alt=""
+        />
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">{/* Product */}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
