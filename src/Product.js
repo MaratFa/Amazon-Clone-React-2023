cd1a85b6920300b25be53732cdffa00efd40b6ff
Marat <evolution99@rambler.ru>
2023-04-19 23:54:43 +0300

    Adding 'Product.js'

---
+++ src/Product.js
@@ -0,0 +1,13 @@
+import "./Product.css";
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>Gaming accessories</p>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
