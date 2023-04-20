36b6815de727524afa54e2bb0676f07c57b7d0c0
Marat <evolution99@rambler.ru>
2023-04-21 02:41:43 +0300

    Added code to 'Home.js'

--- src/Home.js
+++ src/Home.js
@@ -11,15 +11,71 @@ function Home() {
           alt=""
         />
         <div className="home__row">
-          <Product />
-          {/* Product */}
+          <Product
+            id={"1"}
+            title={
+              "SteelSeries Arctis 5 - RGB Illuminated Gaming Headset with DTS Headphone: X v2.0 Surround - for PC and PlayStation 4 - Black"
+            }
+            price={70.99}
+            image={
+              "https://m.media-amazon.com/images/I/81Y9BnR2+hL._AC_SY606_.jpg"
+            }
+            rating={5}
+          />
+          <Product
+            id={"2"}
+            title={
+              "Razer Ornata Chroma Gaming Keyboard: Hybrid Mechanical Key Switches - Customizable Chroma RGB Lighting - Individually Backlit Keys - Detachable Plush Wrist Rest - Programmable Macro Functionality"
+            }
+            price={59.99}
+            image={
+              "https://m.media-amazon.com/images/I/71tDhEbyPRL._AC_SL1500_.jpg"
+            }
+            rating={5}
+          />
         </div>
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+          <Product
+            id={"3"}
+            title={"PlayStation Classic"}
+            price={99.0}
+            image={
+              "https://m.media-amazon.com/images/I/71ogaAK8DGL._AC_SX425_.jpg"
+            }
+            rating={5}
+          />
+          <Product
+            id={"4"}
+            title={"Microsoft Xbox Series S Console - 512GB (Renewed)"}
+            price={264.99}
+            image={
+              "https://m.media-amazon.com/images/I/61Vv5FeZH2L._SL1200_.jpg"
+            }
+            rating={5}
+          />
+          <Product
+            id={"5"}
+            title={
+              "enovo - 2022 - IdeaPad Gaming 3 - Essential Gaming Laptop Computer - 15.6"
+            }
+            price={769.0}
+            image={
+              "https://m.media-amazon.com/images/I/81zcUyiNcUL._AC_SL1500_.jpg"
+            }
+            rating={5}
+          />
         </div>
-        <div className="home__row">{/* Product */}</div>
+        <div className="home__row">
+          <Product
+            id={"6"}
+            title={"Resident Evil 4 - PS5"}
+            price={59.99}
+            image={
+              "https://m.media-amazon.com/images/I/712XPl7+qKL._AC_SL1500_.jpg"
+            }
+            rating={5}
+          />
+        </div>
       </div>
     </div>
   );
