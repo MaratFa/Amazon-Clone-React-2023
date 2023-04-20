d47248a167017b71ef9e517f87fe61bc8bf40195
MaratFa <evolution99@rambler.ru>
2023-04-20 17:27:03 +0300

    Added code to 'Product.js'

--- src/Product.js
+++ src/Product.js
@@ -1,11 +1,23 @@
-import "./Product.css";
+import "./Product.css"
 
 function Product() {
   return (
     <div className="product">
       <div className="product__info">
-        <p>Gaming accessories</p>
+        <p>HyperX Cloud II</p>
+        <p className="product_price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product_rating">
+          <p>&#11088;</p>
+        </div>
       </div>
+      <img 
+        src='https://m.media-amazon.com/images/I/71M5l+O4OFL._AC_SY355_.jpg'
+        alt=""
+      />
+      <button>Add to Basket</button>
     </div>
   );
 }
