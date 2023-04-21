30f41d344363e07e2980477ccccfad89dfb6c556
Marat <evolution99@rambler.ru>
2023-04-21 23:50:17 +0300

    initial state for react-root

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
-import {BrouserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // Bem
     <Router>
       <div className="App">
-        <Switch>
-          <Header />
-          <Home />
-        </Switch>
+        <Header />
+        <Home />
+        <Routes>
+          <Route path="/checkout" element={<h1>fffffff</h1>} />
+        </Routes>
       </div>
     </Router>
   );
