c7707983890e7c6a1895075b9346df9fd0695f54
MaratFa <evolution99@rambler.ru>
2023-04-21 16:45:07 +0300

    Added "react-router-dom"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
+import {BrouserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // Bem
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Header />
+          <Home />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
