f5be48e8932bfd308459e29bcd616c5eb4d102e7
Marat <evolution99@rambler.ru>
2023-04-22 23:52:31 +0300

    Added "HomeMain.js>"

---
+++ src/components/HomeMain.js
@@ -0,0 +1,13 @@
+import Header from "./Header.js";
+import Home from "./Home.js";
+
+const Checkout = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+};
+
+export default Checkout;
