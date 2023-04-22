e1b26e5fe710d964b00ac7d9b506d5e9dcd56192
Marat <evolution99@rambler.ru>
2023-04-22 23:51:13 +0300

    Added "<Checkout.js>"

---
+++ src/components/Checkout.js
@@ -0,0 +1,34 @@
+import "./Checkout.css";
+import Header from "./Header.js";
+import Subtotal from "./Subtotal";
+
+const Checkout = () => {
+  return (
+    <div>
+      <Header />
+      <div className="checkout">
+        <div className="checkout__left">
+          <img
+            className="checkout__ad"
+            src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/MothersDay/Fuji_LP_header_MD21_en_US.png"
+            alt=""
+          />
+          <div>
+            <h2 className="checkout__title">Your shopping basket</h2>
+
+            {/* BasketItem */}
+            {/* BasketItem */}
+            {/* BasketItem */}
+            {/* BasketItem */}
+            {/* BasketItem */}
+          </div>
+        </div>
+        <div className="checkout__right">
+          <Subtotal />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
