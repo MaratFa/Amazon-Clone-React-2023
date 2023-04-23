16546af53ca8857f477ef8d1c0d3b2c8d42c80af
Marat <evolution99@rambler.ru>
2023-04-23 23:32:18 +0300

    Added 'StateProvider'

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { StateProvider } from "./StateProvider";
 import "./index.css";
+import reducer, { initialState } from "./reducer";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
 
