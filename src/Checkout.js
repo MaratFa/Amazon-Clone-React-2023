0d579f9f65b8e4a47590e4b2d0234e417a47c475
Marat <evolution99@rambler.ru>
2023-04-24 06:15:47 +0300

    File location changed

---
+++ src/Checkout.js
@@ -0,0 +1,43 @@
+import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
+import Subtotal from "./Subtotal";
+
+const Checkout = () => {
+  const [{ basket }, dispatch] = useStateValue();
+
+  return (
+    <div>
+      <div className="checkout">
+        <div className="checkout__left">
+          <img
+            className="checkout__ad"
+            src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/MothersDay/Fuji_LP_header_MD21_en_US.png"
+            alt=""
+          />
+          <div>
+            <h2 className="checkout__title">Your shopping basket</h2>
+            {basket.map(item => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+            {/* CheckoutProduct */}
+            {/* CheckoutProduct */}
+            {/* CheckoutProduct */}
+            {/* CheckoutProduct */}
+          </div>
+        </div>
+        <div className="checkout__right">
+          <Subtotal />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
