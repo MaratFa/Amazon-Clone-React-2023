4f4cae93df43762a063fbd9a648e7dc44aa63e4d
Marat <evolution99@rambler.ru>
2023-04-27 07:31:11 +0300

    Added code to 'Login.js'

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
+import { auth } from "./firebase";
 
 const Login = () => {
+  const history = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = e => {
+    e.preventDefault();
+  };
+
+  const register = e => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(auth, email, password)
+      .then(auth => {
+        // It successfully created a new user with email and password
+        console.log(auth);
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch(error => alert(error.message));
+
+    // Do some fancy firebase register
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -13,19 +39,33 @@ const Login = () => {
 
       <div className="login__container">
         <h1>Sign in</h1>
-        <form>
+        <form onSubmit={Login}>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" />
-          <button className="login__signInButton">Sign in</button>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign in
+          </button>
         </form>
         <p>
           By signing-in you agree to the AMAZON FAKE CLONE Condition of Use &
           Sale. Please see our Privacy Notice, our Cookies Notice and our
           Internet-Based Ads Notice.
         </p>
-        <button className="login__registerButton">
+        <button onClick={register} className="login__registerButton">
           Create your Amazon Account
         </button>
       </div>
