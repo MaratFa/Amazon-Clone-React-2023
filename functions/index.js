915a89bbd5c0b50e19de2624ce4a579e172e8b00
Marat <evolution99@rambler.ru>
2023-05-05 01:46:23 +0300

    Corrected error

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")("sk_test_51N2gdpGDWV8jlCoqVeo2duqfAOuDawru4ZShGxfAAlOamwIZJXeB0PYtBkXeYM92Rw7ttK3UZi0GVWTbYe8uXg6P008WtDLqMf");
+const stripe = require("stripe")(
+  "sk_test_51N2gdpGDWV8jlCoqVeo2duqfAOuDawru4ZShGxfAAlOamwIZJXeB0PYtBkXeYM92Rw7ttK3UZi0GVWTbYe8uXg6P008WtDLqMf"
+);
 
 // - API
 
@@ -20,7 +22,7 @@ app.post("/payments/create", async (request, response) => {
 
   console.log("Payment Request Received!! for this amount>>>", total);
 
-  const paymentIntent = await stripe.paymentIntent.create({
+  const paymentIntent = await stripe.paymentIntents.create({
     amount: total, // subunits of the currency
     currency: "usd",
   });
