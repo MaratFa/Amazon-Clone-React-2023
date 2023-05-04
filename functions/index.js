c1d3d998ebff9849f8f76804fbec207eff95f983
Marat <evolution99@rambler.ru>
2023-05-04 23:50:33 +0300

    Reinstalled functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(
-  "sk_test_51N2gdpGDWV8jlCoqVeo2duqfAOuDawru4ZShGxfAAlOamwIZJXeB0PYtBkXeYM92Rw7ttK3UZi0GVWTbYe8uXg6P008WtDLqMf"
-);
+const stripe = require("stripe")("sk_test_51N2gdpGDWV8jlCoqVeo2duqfAOuDawru4ZShGxfAAlOamwIZJXeB0PYtBkXeYM92Rw7ttK3UZi0GVWTbYe8uXg6P008WtDLqMf");
 
 // - API
 
