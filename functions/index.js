c9df2d5f27164cea3455264403b160ba73c04c78
Marat <evolution99@rambler.ru>
2023-05-07 03:57:12 +0300

    Added code to 'index.js'

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51N2gdpGDWV8jlCoqVeo2duqfAOuDawru4ZShGxfAAlOamwIZJXeB0PYtBkXeYM92Rw7ttK3UZi0GVWTbYe8uXg6P008WtDLqMf"
+    "sk_test_51N2gdpGDWV8jlCoqVeo2duqfAOuDawru4ZShGxfAAlOamwIZJXeB0PYtBkXeYM92Rw7ttK3UZi0GVWTbYe8uXg6P008WtDLqMf",
 );
 
 // - API
@@ -11,7 +11,7 @@ const stripe = require("stripe")(
 const app = express();
 
 // - Middlewares
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 app.use(express.json());
 
 // - API routes
