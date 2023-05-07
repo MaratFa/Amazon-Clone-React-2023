9fe9e99a1f84531190ad08d7d296e33cff31ee4a
Marat <evolution99@rambler.ru>
2023-05-07 22:44:40 +0300

    Added code to Orders.js'

--- src/Orders.js
+++ src/Orders.js
@@ -1,56 +1,44 @@
-import { useState } from "react";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import Order from "./Order.js";
 import "./Orders.css";
 import { useStateValue } from "./StateProvider";
+import { db } from "./firebase";
 
 function Orders() {
   const [{ basket, user }, dispatch] = useStateValue();
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState([]); //Initial value will be empty array
+
+  useEffect(() => {
+    if (user) {
+      async function fetchData() {
+        // You can await here
+        const ordersRef = collection(db, "users", user?.uid, "orders");
+        const q = query(ordersRef, orderBy("created", "desc"));
+        const querySnapshot = await getDocs(q);
+        setOrders(
+          querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      }
+      fetchData();
+    } else {
+      setOrders([]);
+    }
+  }, []); //You have to have [] here. This makes it run only once.
 
   return (
     <div className="orders">
       <h1>Your Orders</h1>
       <div className="orders__order">
-        {orders?.map(order => (
-          <Order order={order} />
-        ))}
+        {orders?.map(order => {
+          <Order order={order} />;
+        })}
       </div>
     </div>
   );
 }
 
 export default Orders;
-
-/*   
-
-
-
-I think your question is regarding this part?
-
-db
-  .collection('users')
-  .doc(user?.uid)
-  .collection('orders')
-  .orderBy('created', 'desc')
-  .onSnapshot(snapshot => (
-    setOrders(snapshot.docs.map(doc => ({
-      id:doc.id,
-      data:doc.data()
-    })))
-  ))
-
-There is an onSnapshot() method, please check documentation for orderBy and onSnapshot. Code should be something like this:
-
-if(user?.uid) {
-  const ordersRef = collection(db, "users", user.uid, orders);
-  const q = query(ordersRef, orderBy('created', 'desc'));
-  onSnapshot(q, (querySnapshot) => {
-  setOrders(querySnapshot.docs.map(doc => ({
-    id:doc.id,
-    data:doc.data()
-  })))
-});
-}
-
-
-  */
