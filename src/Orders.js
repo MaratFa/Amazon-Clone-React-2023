e5e4f574f92a0fbb0fbc6d331cb0867f08b444e2
Marat <evolution99@rambler.ru>
2023-05-09 04:57:44 +0300

    'Your orders' Page

--- src/Orders.js
+++ src/Orders.js
@@ -1,41 +1,40 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import Order from "./Order.js";
+import Header from "./Header.js";
+import Order from "./Order";
 import "./Orders.css";
 import { useStateValue } from "./StateProvider";
 import { db } from "./firebase";
 
 function Orders() {
   const [{ basket, user }, dispatch] = useStateValue();
-  const [orders, setOrders] = useState([]); //Initial value will be empty array
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     if (user) {
-      async function fetchData() {
-        // You can await here
-        const ordersRef = collection(db, "users", user?.uid, "orders");
-        const q = query(ordersRef, orderBy("created", "desc"));
-        const querySnapshot = await getDocs(q);
+      const ref = collection(db, "users", "orders", user?.uid);
+      const orderedOrders = query(ref, orderBy("created", "desc"));
+      onSnapshot(orderedOrders, snapshot => {
         setOrders(
-          querySnapshot.docs.map(doc => ({
+          snapshot.docs.map(doc => ({
             id: doc.id,
             data: doc.data(),
           }))
         );
-      }
-      fetchData();
+      });
     } else {
       setOrders([]);
     }
-  }, []); //You have to have [] here. This makes it run only once.
-
+  }, [user]);
+  debugger;
   return (
     <div className="orders">
+      <Header />
       <h1>Your Orders</h1>
       <div className="orders__order">
-        {orders?.map(order => {
-          <Order order={order} />;
-        })}
+        {orders?.map(order => (
+          <Order order={order} />
+        ))}
       </div>
     </div>
   );
